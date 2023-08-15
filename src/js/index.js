77e2ad96c43717f2a5cdb4f87b007cda8354f462
ometman <revomet@gmail.com>
2023-08-15 16:42:50 +0000

    Check linters

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,13 @@
-import displayAllTasks from '../modules/displayList';
 // Import our custom CSS
 import '../scss/styles.scss';
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap';
 
+import displayAllTasks from '../modules/displayList.js';
+
 window.onload = () => {
-  displayAllTasks()
+  displayAllTasks();
 };
 
 export { bootstrap as default };
