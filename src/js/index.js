f2eb29f65a4adf217f6042ae7a79d904e6e2c443
ometman <revomet@gmail.com>
2023-08-15 16:39:18 +0000

    Add function in index.js to populate task dynamically

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,12 @@
+import displayAllTasks from '../modules/displayList';
 // Import our custom CSS
 import '../scss/styles.scss';
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap';
 
-
+window.onload = () => {
+  displayAllTasks()
+};
 
 export { bootstrap as default };
