bc687130671f18a4d2dc6ccc2ec217505df75d5d
ometman <revomet@gmail.com>
2023-08-16 16:03:52 +0000

    Implement add and remove functionality

--- src/js/index.js
+++ src/js/index.js
@@ -4,10 +4,34 @@ import '../scss/styles.scss';
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap';
 
-import displayAllTasks from '../modules/displayList.js';
+import { displayTasks } from '../modules/showTasks.js';
+import { addNewTask } from '../modules/addTask.js';
+import { removeTask } from '../modules/removeTask.js';
+import { changeIcon } from '../modules/changeIcon.js';
 
 window.onload = () => {
-  displayAllTasks();
+  // refresh page by refresh icon btn
+  const refreshPage = document.querySelector('#refresh-page-btn');
+  refreshPage.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.location.reload();
+  });
+
+  // display tasks
+  displayTasks();
+
+  // task removal
+  const removeBtn = document.querySelectorAll('.remove-btn');
+  removeBtn.forEach((btn, btnIndex) => {
+    btn.addEventListener('click', () => {
+      removeTask(btnIndex);
+    });
+  });
+
+  // adding new task
+  addNewTask();
+  // change icon on hover
+  changeIcon();
 };
 
 export { bootstrap as default };
