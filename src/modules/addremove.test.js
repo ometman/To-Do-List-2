f05c45cad500894a8e1b304046bc24045f5f870a
ometman <revomet@gmail.com>
2023-08-30 09:00:50 +0000

    Add test code for task event call

--- src/modules/addremove.test.js
+++ src/modules/addremove.test.js
@@ -4,11 +4,27 @@
 import { TasksClass } from './tasksClass.js';
 
 describe('adding and removing a task', () => {
-    const addClass = new TasksClass();
-    const addNewTask = () => {
-        document.body.innerHTML = `
+  const addClass = new TasksClass();
+  const addNewTask = () => {
+    document.body.innerHTML = `
         <input id="task-input" type="text" />
         <button id="task-input-return" type="submit"></button>`;
-    }
-addNewTask();
-})
+  };
+  addNewTask();
+
+  test('successful add task event call', () => {
+    const taskBtn = document.querySelector('#task-input-return');
+    const taskInput = document.querySelector('#task-input');
+    taskInput.value = 'taskname';
+    taskBtn.click();
+    return Promise.resolve()
+      .then(async () => {
+        expect(taskBtn).not.toBeNull();
+      })
+      .then(() => {
+        expect(taskInput.value).not.toBeNull();
+        expect(taskInput.value).toBe('taskname');
+      });
+  });
+  
+});
