02db55bf7b423be8c66992b1ba9ba677f3d0fc9d
ometman <revomet@gmail.com>
2023-08-30 09:09:45 +0000

    Add test code for class method call for add and remove task

--- src/modules/addremove.test.js
+++ src/modules/addremove.test.js
@@ -11,7 +11,6 @@ describe('adding and removing a task', () => {
         <button id="task-input-return" type="submit"></button>`;
   };
   addNewTask();
-  addClass.addATask();
 
   test('successful add task event call', () => {
     const taskBtn = document.querySelector('#task-input-return');
@@ -29,5 +28,73 @@ describe('adding and removing a task', () => {
   });
   test('successful add task class and method calls', () => {
     expect(addClass).toBeInstanceOf(TasksClass);
+  });
+
+  const addTaskMock = jest
+    .spyOn(addClass, 'addATask')
+    .mockImplementation(() => 'mocked-addATask-method');
+
+  test('successful add task class and method calls', () => {
+    const taskInput = document.querySelector('#task-input');
+    taskInput.value = 'taskname';
+    const taskBtn = document.querySelector('#task-input-return');
+    addClass.addATask('taskInput.value');
+    taskBtn.click();
+    return Promise.resolve()
+      .then(async () => {
+        expect(addTaskMock).toHaveBeenCalled();
+      })
+  });
+
+  const taskRemover = (elLists, elId) => {
+    const elLength = 6;
+    elLists = new Array(elLength);
+    let i = 0;
+    elId = i;
+
+    for (let i = 1; i <= elLists.length; i += 1) {
+      document.createElement('div').innerHTML = `
+      <button id="${i}" class="remove-btn" type="submit"></button>`;
+    }
+    document.body.appendChild()
+  };
+  taskRemover();
+  // taskRemover = (btnLists, id) => {
+  //   const ttC = this.taskCollection;
+  //   Array.prototype.forEach.call(btnLists, (btnList, btnIndex) => {
+  //     btnList.addEventListener('click', () => {
+  //       if (btnIndex === id) {
+  //         ttC.splice(btnIndex, 1);
+  //         ttC.sort((task1, task2) => task1.taskIndex - task2.taskIndex);
+  //         ttC.forEach((taskItem, taskItemIndex) => {
+  //           taskItem.taskIndex = taskItemIndex + 1;
+  //         });
+  //         localStorage.setItem('taskList', JSON.stringify(ttC));
+  //         this.displayAllTasks();
+  //       }
+  //     });
+  //   });
+  // }
+
+
+
+
+
+
+
+
+  test('successful remove task event call', () => {
+    const taskBtn = document.querySelector('#task-input-return');
+    const taskInput = document.querySelector('#task-input');
+    taskInput.value = 'taskname';
+    taskBtn.click();
+    return Promise.resolve()
+      .then(async () => {
+        expect(taskBtn).not.toBeNull();
+      })
+      .then(() => {
+        expect(taskInput.value).not.toBeNull();
+        expect(taskInput.value).toBe('taskname');
+      });
   });
 });
