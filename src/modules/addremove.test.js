1bcd7f55e135381b6ef0d63155383dc1de277eb9
Samuel T. Boateng <samueltakyiboateng@gmail.com>
2023-08-30 03:57:45 -0500

    add html mock to test file

--- src/modules/addremove.test.js
+++ src/modules/addremove.test.js
@@ -1,4 +1,14 @@
 /**
  * @jest-environment jsdom
  */
-// import { TasksClass } from './tasksClass.js';
+import { TasksClass } from './tasksClass.js';
+
+describe('adding and removing a task', () => {
+    const addClass = new TasksClass();
+    const addNewTask = () => {
+        document.body.innerHTML = `
+        <input id="task-input" type="text" />
+        <button id="task-input-return" type="submit"></button>`;
+    }
+addNewTask();
+})
