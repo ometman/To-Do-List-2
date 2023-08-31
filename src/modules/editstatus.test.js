958a6e82c216d04b6f8d6396264f9a9187b39523
ometman <revomet@gmail.com>
2023-08-31 12:49:55 +0000

    Create test file for edit and status

---
+++ src/modules/editstatus.test.js
@@ -0,0 +1,26 @@
+/**
+ * @jest-environment jsdom
+ */
+import { TasksClass } from './tasksClass.js';
+
+describe('editing, updating status and clearing completed task', () => {
+  const testClass = new TasksClass();
+  const taskArr = [
+    {
+      index: 1,
+      description: 'coding practice',
+      completion: false,
+    },
+    {
+      index: 2,
+      description: 'washing',
+      completion: false,
+    },
+  ];
+  const editTask = () => {
+    document.body.innerHTML = `
+     <ul>
+     <li id="task-text" contenteditable="true" >${taskArr[0].description}</li>
+     ul`;
+  };
+});
