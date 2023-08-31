44e596e25939b413751619fde0f0e6d91ff64716
Samuel TB <samueltakyiboateng@gmail.com>
2023-08-31 08:23:37 -0500

    modify test code to check editable elements

--- src/modules/editstatus.test.js
+++ src/modules/editstatus.test.js
@@ -1,41 +1,49 @@
 /**
  * @jest-environment jsdom
  */
-import { TasksClass } from './tasksClass.js';
+import { TasksClass } from "./tasksClass.js";
 
-describe('editing, updating status and clearing completed task', () => {
-  const testClass = new TasksClass();
-  const taskArr = [
-    {
-      index: 1,
-      description: 'coding practice',
-      completion: false,
-    },
-    {
-      index: 2,
-      description: 'washing',
-      completion: false,
-    },
-  ];
-  const editTask = () => {
-    document.body.innerHTML = `
+describe("editing, updating status and clearing completed task", () => {
+	const testClass = new TasksClass();
+	const taskArr = [
+		{
+			index: 1,
+			description: "coding practice",
+			completion: false,
+		},
+		{
+			index: 2,
+			description: "washing",
+			completion: false,
+		},
+	];
+	const editTask = () => {
+		document.body.innerHTML = `
      <ul>
      <li id="task-text" contenteditable="true" >${taskArr[0].description}</li>
      ul`;
-  };
+	};
 
-  test('editable element and event call', () => {
-    editTask();
-    const taskEl = document.querySelector('#task-text');
-    taskEl.click();
-    return Promise.resolve()
-      .then(async () => {
-        expect(taskEl.innerText).not.toBe(null);
-        expect(taskEl.isContentEditable).toBe(true);
-      });
-  });
+	test("editable element and event call", () => {
+		editTask();
+		const taskEl = document.querySelector("#task-text");
+		taskEl.click();
+		return Promise.resolve().then(async () => {
+			expect(taskEl.innerText).not.toBe(null);
+			expect(taskEl.isContentEditable).toBe(true);
+		});
+	});
 
-  test('successful edit task class instance call', () => {
-    expect(testClass).toBeInstanceOf(TasksClass);
-  });
+	test("successful edit task class instance call", () => {
+		expect(testClass).toBeInstanceOf(TasksClass);
+	});
+
+	test("test editable task and event call", () => {
+		editTask();
+		const taskEl = document.querySelector("#task-text").isContentEditable;
+		taskEl.click();
+		return Promise.resolve().then(async () => {
+			expect(taskEl.isContentEditable).toBe(true);
+		});
+	});
 });
